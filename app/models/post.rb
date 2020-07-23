dc492f9a691f31e5a6e8cdef86826b650cc11efe
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-23 01:39:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title presence: true 
+  validates :title, presence: true 
   validates :category 
-  validates :content length: { minimum: 100}
+  validates :content, length: { minimum: 100}
 end
