8103337627c2a8dfcde18c80623b8d2e5adc5e77
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-23 01:47:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
-  validates_with CategoryValidator 
+  validates :category, inclusion: 
   validates :content, length: { minimum: 100 }
 end
 
-class CategoryValidator < ActiveModel::Validator
-  def validate(category)
-    if Post.category == "Fiction" || Post.category == "NonFiction" 
-      true 
-    else 
-      false 
-    end 
-  end 
-end 
