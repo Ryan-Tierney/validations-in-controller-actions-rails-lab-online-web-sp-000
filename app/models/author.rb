c207eaa6cc102b52b753c8067449576b64e1bea4
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-23 01:56:13 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates_presence_of :name, :email
-  validates :email, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, uniqueness: true
+
 end 
